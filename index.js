6c8aff3e9d7e2805e6af221ea2a480815cc79a3e
ikasraa <kasraaky@gmail.com>
2024-12-08 09:43:48 +0330

    social media
    ezafe kardane parte social media be main page

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const sidebarToggle = document.getElementById('sidebar-toggle-btn')
+const body = document.querySelector("body"),
+    modeToggle = body.querySelector(".mode-toggle");
+    sidebar = body.querySelector("nav");
+    sidebarToggle = body.querySelector(".sidebar-toggle");
+
+modeToggle.addEventListener("click", () => { 
+    body.classList.toggle("dark");
+});
 
 sidebarToggle.addEventListener("click" , () =>{
     sidebar.classList.toggle("close");
